6903216196f328fb37b4eb3d22c41494a264242b
Brassco <tonny.brassco@gmail.com>
2024-03-02 12:22:39 +0200

    Added Button component and unit tests for it

---
+++ src/components/Button/ButtonComponent.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//Types
+export type ButtonProps = {
+  title: string;
+  onPress: () => void;
+};
+
+//View
+import ButtonView from './ButtonView';
+
+const ButtonComponent = ({title, onPress}: ButtonProps) => {
+  return <ButtonView title={title} onPress={onPress} />;
+};
+
+export default ButtonComponent;
---
+++ src/components/Button/__tests__/ButtonComponent.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react-native';
+import ButtonComponent, { ButtonProps } from '../ButtonComponent';
+
+describe('ButtonComponent', () => {
+  test('renders with the correct title', () => {
+    const title = 'Click me';
+    const { getByText } = render(<ButtonComponent title={title} onPress={() => {}} />);
+    const buttonElement = getByText(title);
+    expect(buttonElement).toBeDefined();
+  });
+
+  test('calls onPress when the button is pressed', () => {
+    const onPressMock = jest.fn().mockReturnValue(1);
+    const { getByText } = render(<ButtonComponent title="Click me" onPress={onPressMock} />);
+    const buttonElement = getByText('Click me');
+    
+    fireEvent.press(buttonElement);
+    
+    // Check if onPress is called once
+    expect(onPressMock).toHaveBeenCalledTimes(1);
+    
+    // If your onPress function returns a value, you can check it
+    expect(onPressMock).toHaveReturnedWith(1);
+  });
+});
